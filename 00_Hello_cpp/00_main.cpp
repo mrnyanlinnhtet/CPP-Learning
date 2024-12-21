ea8e660adcac24d0299bd9c8f2ddfb1e9c6d541a
mrnyanlinnhtet18 <nyan@mymyeo.com>
2024-12-22 01:35:36 +0630

    helo-s-io

---
+++ 00_Hello_cpp/00_main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello C++ Programming" << endl;
+    return 0;
+}
---
+++ 01_Input_output/01_input_output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int userInput;
+
+    cout<<"Please Enter Your Number : ";
+    cin>>userInput;
+    cout<<"Your Input Number is : "<<userInput;
+
+    return 0;
+}
